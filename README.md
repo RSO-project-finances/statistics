59410913d84d826eff02de6a9166c540e75ec9cf
Tomaz12345 <tomazpoljansek12@gmail.com>
2024-01-18 18:40:32 +0100

    added things

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # statistics
 microservice statistics
+
+```bash
+mvn clean package
+docker build -t statistics .   
+docker tag statistics tomaz12345/statistics   
+docker push tomaz12345/statistics   
+docker network ls  
+docker network rm rso
+docker network create rso
+docker run -d --name pg-expenses -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=expenses -p 5432:5432 --network rso postgres:13 #docker inspect pg-expenses
+docker run -p 8081:8081 --network rso -e KUMULUZEE_DATASOURCES0_CONNECTIONURL=jdbc:postgresql://pg-expenses:5432/expenses tomaz12345/statistics
+```
--- api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/ExpensesApplication.java
+++ api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/ExpensesApplication.java
@@ -10,10 +10,10 @@ import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
-@OpenAPIDefinition(info = @Info(title = "Expenses API", version = "v1",
+@OpenAPIDefinition(info = @Info(title = "Statistics API", version = "v1",
         contact = @Contact(email = "tomazpoljansek12@gmail.com"),
-        license = @License(name = "dev"), description = "API for managing expenses."),
-        servers = @Server(url = "http://localhost:8081/"))
+        license = @License(name = "dev"), description = "API for getting expenses statistics."),
+        servers = @Server(url = "http://20.228.195.151/v1/"))
 @ApplicationPath("/v1")
 public class ExpensesApplication extends Application {
 
